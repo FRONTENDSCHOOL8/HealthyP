c213d944ed773ab5991766622199e4cd1f696adc
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-02-22 23:11:35 +0900

    [Init]
    초기설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { module: true, browser: true, es2021: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000, // default: 5173
     open: true, // default: false
-  },
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
   },
   css: {
     devSourcemap: true,
