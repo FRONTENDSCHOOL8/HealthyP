d889b9a5b05934975b4cbcab0705f7f322a6fbee
dldnlee <sungeun8877@gmail.com>
2024-02-26 15:20:02 +0900

    [ Init ] : - .env 파일 추가 - vite-env.d.ts 설정 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
+// vite-env.d.ts 파일
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PB_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
