4585a2e5a3fbc3fef00e42db0898df1a042582e3
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-02-29 00:03:02 +0900

    [feat] RecipeCard 컴포넌트 만드는중 main에서 작업중... medal 파일 추가 pb에서 이미지 url 가져오는 로직 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,16 +1,26 @@
-import Star from '@/components/star/Star';
+import { pb } from '@/api/pocketbase';
+import RecipeCard from '@/components/recipeCard/RecipeCard';
 import { getDataAtomFamily } from '@/util';
 import { useAtom } from 'jotai';
+import { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 export function MainPage() {
+  const urls = useRef(null);
   const [{ data, loading, error }] = useAtom(
     getDataAtomFamily({
       item: 'recipes',
-      typeOfGetData: 'getOne',
+      typeOfGetData: 'getFullList',
       options: { expand: 'rating' },
-      setting: '5p9kta3jipwrunq',
     })
   );
+
+  if (data) {
+    const urlArr = data.map((data: object) =>
+      pb.files.getUrl(data, data?.image)
+    );
+    urls.current = urlArr;
+  }
 
   if (loading)
     return (
@@ -25,10 +35,33 @@ export function MainPage() {
         <p>에러</p>
       </div>
     );
-
+  console.log(data);
   return (
     <div>
-      {/* <Star rating={data?.expand.rating} /> */}
+      <section>
+        <Link
+          to={'bookmark'}
+          className="flex pl-14pxr pr-10pxr py-10pxr text-title-2-em justify-between"
+        >
+          <h2>오늘의 레시피</h2>
+          <span className="size-30pxr bg-arrow-small-icon rotate-[270deg] bg-center"></span>
+        </Link>
+        <div className="w-full overflow-x-scroll">
+          <div className="flex gap-2 px-side w-max pb-2">
+            {data &&
+              data.map(({ id, title, expand }, idx) => {
+                return (
+                  <RecipeCard
+                    key={id}
+                    title={title}
+                    url={urls?.current[idx]}
+                    rating={expand.rating}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
