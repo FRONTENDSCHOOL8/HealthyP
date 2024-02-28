b1f2502420a572bdb66740dfeaf0c7eebb48d07a
dldnlee <sungeun8877@gmail.com>
2024-02-28 21:25:05 +0900

    [Modify] - pocketbase types 추가 - getDataAtomFamily 코드 pb에서 db로 수정

--- src/util/data/getDataAtomFamily.ts
+++ src/util/data/getDataAtomFamily.ts
@@ -1,72 +1,58 @@
-import { pb } from '@/api/pocketbase';
+import { db } from '@/api/pocketbase';
 import { getDataInterface } from '@/types';
 import { atom } from 'jotai';
 import { atomFamily } from 'jotai/utils';
 import isEqual from 'lodash/isEqual';
-import { RecipesResponse, CollectionResponses } from '@/types/pocketbase-types';
-import { ListResult } from 'pocketbase';
+import { DataState } from '@/types';
 
-type Tmetadata = {
-  likes: number;
-};
-type Texpand = {
-  user: UsersResponse;
-};
-type stateType = {
-  data:
-    | RecipesResponse<Tmetadata, Texpand>
-    | RecipesResponse<Tmetadata, Texpand>[]
-    | ListResult<RecipesResponse<Tmetadata, Texpand>>
-    | null;
-  loading: boolean;
-  error?: 'string' | null;
-};
+
 
 export const getDataAtomFamily = atomFamily((params: getDataInterface) => {
   const dataFunctionsMap = {
     getFullList: async () => {
       const { item, options } = params;
-      return pb
+      return db
         .collection(item)
-        .getFullList<CollectionResponses<Tmetadata, Texpand>>(options);
+        .getFullList(options);
     },
     getList: async () => {
       const { item, options, setting } = params;
       const page = typeof setting === 'number' ? setting : undefined;
-      return pb
+      return db
         .collection(item)
-        .getList<CollectionResponses<Tmetadata, Texpand>>(1, page, options);
+        .getList(1, page, options);
     },
     getOne: async () => {
       const { item, options, setting } = params;
       const id = typeof setting === 'string' ? setting : '';
-      return pb
+      console.log('test');
+      return db
         .collection(item)
-        .getOne<CollectionResponses<Tmetadata, Texpand>>(id, options);
+        .getOne(id, options);
     },
     getFirstListItem: async () => {
       const { item, options, setting } = params;
       const filter = typeof setting === 'string' ? setting : '';
-      return pb
+      return db
         .collection(item)
-        .getFirstListItem<
-          CollectionResponses<Tmetadata, Texpand>
-        >(filter, options);
+        .getFirstListItem(filter, options);
     },
   };
 
   return atom(async () => {
-    const state: stateType = {
+
+    const state: DataState<object> = {
       data: null,
       loading: true,
       error: null,
-    };
+    }
 
     try {
       const { typeOfGetData } = params;
       const dataFunction = dataFunctionsMap[typeOfGetData];
       if (dataFunction) {
         state.data = await dataFunction();
+        
       } else {
         console.error(`지원하지 않는 typeOfGetData 입니다: ${typeOfGetData}`);
       }
@@ -74,11 +60,15 @@ export const getDataAtomFamily = atomFamily((params: getDataInterface) => {
       if (error instanceof Error && error.name !== 'AbortError') {
         console.error('데이터를 불러오는데 실패했습니다.', error);
         state.error;
-      }
-    } finally {
+      } 
+    }
+    finally {
       state.loading = false;
     }
 
     return state;
   });
 }, isEqual);
+
+
+
