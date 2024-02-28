fdf9666ae3d9a455a8cef55705d6a5c28b02b7b9
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-02-28 15:30:40 +0900

    [Feat]
    - separateComma 유틸함수 추가

--- src/pages/bookmark/index.tsx
+++ src/pages/bookmark/index.tsx
@@ -1,8 +1,13 @@
 import Review from "@/components/reviews/Review";
+import separateComma from "@/util/separateComma";
 
 
 
 export function BookmarkPage() {
+  const itemsString = "쌀국수, 고수가좋아,고수가고수처럼고수먹방"
+  const itemsArray = separateComma(itemsString);
+
+  console.log(itemsArray);
   return (
     <Review id="ka5sini1xvt9itq" caseType="literal"/>
   )
