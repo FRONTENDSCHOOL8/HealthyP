3f5e05f0b25c6268cab311839c0588a2c484b5be
dldnlee <sungeun8877@gmail.com>
2024-02-29 17:10:03 +0900

    [Feat] - InputComponent fileInput옵션 추가

--- src/components/input/InputComponent.tsx
+++ src/components/input/InputComponent.tsx
@@ -2,10 +2,11 @@ import { useAtom } from "jotai";
 import { useState } from "react";
 import { passwordAtom } from "@/pages";
 interface InputProps {
-  option: "search" | "email" | "password" | "nickname" | "passwordConfirm";
+  option: "search" | "email" | "password" | "nickname" | "passwordConfirm" | "fileInput";
   placeholder?: string;
   bgColor?: string;
   changeHandler?: () => void;
+  inputTitle?: string;
 }
 
 function pwReg(text:string){
@@ -13,7 +14,7 @@ function pwReg(text:string){
   return re.test(String(text).toLowerCase());
 }
 
-function useInputMapping({option, placeholder, bgColor} : InputProps) {
+function useInputMapping({option, placeholder, bgColor, inputTitle} : InputProps) {
   const [pwBorder, setPwBorder] = useState('');
   const [confirmBorder, setConfirmBorder] = useState('');
   const [password, setPassword] = useAtom(passwordAtom);
@@ -80,9 +81,23 @@ function useInputMapping({option, placeholder, bgColor} : InputProps) {
     ),
     nickname: (
       <>
-        <label htmlFor="nickname-input" className="text-gray-500 text-foot-em">닉네임
+        <label htmlFor="nickname-input" className="text-sub-em flex flex-col gap-10pxr">닉네임
           <input id="nickname-input" type="text" className="w-full h-48pxr py-0 px-10pxr bg-gray_150 rounded-md" placeholder={placeholder} />
         </label>
+      </>
+    ),
+    fileInput: (
+      <>
+          <label htmlFor="dropzone-file" className="text-sub-em flex flex-col gap-10pxr">
+            <p className="inline-block">{inputTitle}<span className="text-sub">{'(필수)'}</span></p>
+            <div className="h-180pxr w-full bg-gray-300 hover:bg-gray-300 flex justify-center items-center hover:fill-gray-500 fill-gray-400 rounded-lg ">
+              <svg width="48" height="48" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="12" r="9.75" className="fill-inherit"/>
+                <path d="M11.351 12.6365H6.35099C6.16711 12.6365 6.01297 12.5748 5.88857 12.4513C5.76417 12.3278 5.70197 12.1732 5.70197 11.9874C5.70197 11.8016 5.76417 11.647 5.88857 11.5236C6.01297 11.4002 6.16711 11.3385 6.35099 11.3385H11.351V6.33848C11.351 6.15459 11.4134 6.00045 11.5382 5.87605C11.663 5.75165 11.8176 5.68945 12.0021 5.68945C12.1866 5.68945 12.3406 5.75165 12.4639 5.87605C12.5873 6.00045 12.649 6.15459 12.649 6.33848V11.3385H17.649C17.8329 11.3385 17.9871 11.4009 18.1115 11.5258C18.2359 11.6507 18.2981 11.8054 18.2981 11.99C18.2981 12.1774 18.2359 12.332 18.1115 12.4538C17.9871 12.5756 17.8329 12.6365 17.649 12.6365H12.649V17.6365C12.649 17.8232 12.5866 17.9781 12.4618 18.1011C12.337 18.2241 12.1824 18.2856 11.9979 18.2856C11.8134 18.2856 11.6595 18.2241 11.5361 18.1011C11.4127 17.9781 11.351 17.8232 11.351 17.6365V12.6365Z" fill="white"/>
+              </svg>
+            </div>
+            <input id="dropzone-file" type="file" className="hidden" />
+          </label>
       </>
     )
   }
@@ -93,8 +108,8 @@ function useInputMapping({option, placeholder, bgColor} : InputProps) {
   }
 }
 
-export default function InputComponent({option, placeholder, bgColor} : InputProps) {
-  const inputComponent = useInputMapping({option, placeholder, bgColor})
+export default function InputComponent({option, placeholder, bgColor, inputTitle} : InputProps) {
+  const inputComponent = useInputMapping({option, placeholder, bgColor, inputTitle})
 
 
   if (!inputComponent) {
