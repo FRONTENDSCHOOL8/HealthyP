b07c5d1c26d335a0d76f05e26cc85fa91e7d148c
dldnlee <sungeun8877@gmail.com>
2024-02-29 17:11:27 +0900

    [Modify] - InputComponent 새로운 옵션 호출 코드 추가

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,10 @@
 import { Provider, atom } from 'jotai';
 import InputComponent from '@/components/input/InputComponent';
-import { FnButton } from '@/components/buttons/FnButton';
-import { useNavigate } from 'react-router-dom';
-import arrowBig from '@/assets/icons/arrowBig.svg';
+// import { FnButton } from '@/components/buttons/FnButton';
 import Header from '@/components/header/Header';
 // import Header from '@/components/header/Header';
+import { Form } from 'react-router-dom';
+
 export const passwordAtom = atom('');
 
 export default function SearchPage() {
@@ -17,18 +17,21 @@ export default function SearchPage() {
 
 function SearchPageContent() {
   // const [password] = useAtom(passwordAtom);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <div className="w-full relative">
-      <InputComponent option="password" />
-      <FnButton 
+      {/* <FnButton 
           image={arrowBig} 
           clickHandler={() => navigate(-1)}
           altText="뒤로가기"
-          size={30} />
+        size={30} /> */}
 
       <Header option="onlyArrow" />
+      <Form action='/'>
+        <InputComponent option="fileInput" inputTitle="레시피 사진" />
+        <InputComponent option="nickname" />
+      </Form>
     </div>
   );
 }
