b97e55cf3a85c084f017c1d40ebda513fddee044
dldnlee <sungeun8877@gmail.com>
2024-03-02 21:08:47 +0900

    [Feat] - CreateOne 페이지 마크업 완료

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -5,13 +5,67 @@ export { CreateTwo } from './CreateTwo';
 export { CreateThree } from './CreateThree';
 export { CreateComplete } from './CreateComplete';
 
-export function CreateLayout() {
-  
+import { atom, Provider, useAtom } from 'jotai';
 
+
+const temp_data = [
+  {
+    id : "1",
+    ingredient_name : "감자",
+    ingredient_amount : "100g, 3개"
+  },
+  {
+    id : "2",
+    ingredient_name : "고구마",
+    ingredient_amount : "10g, 3개"
+  },
+  {
+    id : "3",
+    ingredient_name : "가지",
+    ingredient_amount : "100g, 3개"
+  },
+  {
+    id : "4",
+    ingredient_name : "양파",
+    ingredient_amount : "100g, 3개"
+  },
+]
+
+const temp_data_two = [
+  {
+    id : "1",
+    ingredient_name : "고추장",
+    ingredient_amount : "100g, 3개"
+  },
+  {
+    id : "2",
+    ingredient_name : "된장",
+    ingredient_amount : "10g, 3개"
+  },
+  {
+    id : "3",
+    ingredient_name : "간장",
+    ingredient_amount : "100g, 3개"
+  },
+  {
+    id : "4",
+    ingredient_name : "양파",
+    ingredient_amount : "100g, 3개"
+  },
+]
+
+export const ingredients = atom(temp_data);
+export const seasoning = atom(temp_data_two);
+
+export function CreateLayout() {
+  const [ingredientData, setIngredientData] = useAtom(ingredients);
+  const [seasoningData, setSeasoningData] = useAtom(seasoning);
 
   return (
     <div className="w-full h-full relative bg-white">
-      <Outlet />
+      <Provider>
+        <Outlet />
+      </Provider>
     </div>
   );
 }
