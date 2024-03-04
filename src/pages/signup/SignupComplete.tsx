67a595d73c1cd245ade24ffb8e3121c1cf606ab2
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-03-05 00:34:54 +0900

    [Feat]
    - openAI api 를 이용한 영양정보 획득
    - openai 라이브러리 설치

--- src/pages/signup/SignupComplete.tsx
+++ src/pages/signup/SignupComplete.tsx
@@ -1,30 +1,5 @@
-import { useState } from 'react';
-import { fetchNutritionInfo } from '@/util/data/gpt';
+import main from '@/util/data/gpt';
 
 export function SignupComplete() {
-  const [query, setQuery] = useState('');
-  const [answer, setAnswer] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetchNutritionInfo(
-      `영양 정보에 대해 알려주세요: ${query}`
-    );
-    setAnswer(response);
-  };
-
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="질문을 입력하세요..."
-        />
-        <button type="submit">물어보기</button>
-      </form>
-      <p>답변: {answer}</p>
-    </div>
-  );
+  main();
 }
--- src/util/data/gpt.ts
+++ src/util/data/gpt.ts
@@ -1,20 +1,28 @@
-export const fetchNutritionInfo = async (query) => {
-  const response = await fetch(
-    'https://api.openai.com/v1/engines/davinci/completions',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content:
+          '너는 우리가 주는 정보들을 바탕으로 해당 식품의 영양정보를 알려주는 도우미야. 예를 들어, 감자 3개 라고 하면 감자3개에 해당하는 영양정보를 알려주면돼. 탄수화물, 단백질, 지방, 칼로리에 대한 정보를 말이야. 이걸 JSON 형식으로 알려주면 돼. 이해했어?',
       },
-      body: JSON.stringify({
-        prompt: query,
-        max_tokens: 150,
-      }),
-    }
-  );
+      {
+        role: 'user',
+        content:
+          '감자3개, 당근2개, 양파1개, 고추장1개 의 영양정보를 알려주면 좋겠어.',
+      },
+    ],
+    model: 'gpt-3.5-turbo-0125',
+    response_format: { type: 'json_object' },
+  });
+  console.log(completion.choices[0].message.content);
+}
 
-  const data = await response.json(); // 응답 데이터를 JSON 형태로 변환
-
-  return data.choices[0].text.trim(); // 변환된 데이터에서 원하는 정보 추출
-};
+export default main;
